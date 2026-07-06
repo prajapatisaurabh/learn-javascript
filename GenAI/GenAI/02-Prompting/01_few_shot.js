a04c8170d6b88e63381b5f29cf301ed72afbf353
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2026-07-06 18:58:32 +0530

    Add initial project files and update .gitignore for Node.js environment

---
+++ GenAI/GenAI/02-Prompting/01_few_shot.js
@@ -0,0 +1,21 @@
+import { OpenAI } from "openai";
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function main() {
+  const response = await client.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "user",
+        content: "tell me a story about little red riding hood ",
+      },
+    ],
+  });
+
+  console.log(response.choices[0].message.content);
+}
+
+main();
---
+++ GenAI/GenAI/02-Prompting/prompt.md
@@ -0,0 +1,5 @@
+## Promping
+
+1. Zero Shot Prompting - Direct Instruction
+2. Few Shot Prompoting - Direct Instructions but with some example
+3. Chain of Thought - give some example and get output
